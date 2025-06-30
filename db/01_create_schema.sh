0430ec5625c523f5201161a3dcc69cd0d55def99
ERuchiyama <akito.uchiyama@ever-rise.co.jp>
2025-06-30 13:17:55 +0900

    fix: coderabbitのレビュー修正

--- db/01_create_schema.sh
+++ db/01_create_schema.sh
@@ -1,3 +1,6 @@
-# !/bin/sh
+#!/bin/sh
 
-psql -U $POSTGRES_USER -d $POSTGRES_DB -c "CREATE SCHEMA ${TODO_SCHEMA};"
+psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE SCHEMA IF NOT EXISTS ${TODO_SCHEMA};" || {
+    echo "Failed to create schema ${TODO_SCHEMA}" >&2
+    exit 1
+}
