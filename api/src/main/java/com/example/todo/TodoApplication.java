88fc053b3d193499ab1dde7aa62dc93d0215f1de
ERuchiyama <akito.uchiyama@ever-rise.co.jp>
2025-06-30 13:32:18 +0900

    fix: 静的解析の結果を修正・設定修正

--- api/src/main/java/com/example/todo/TodoApplication.java
+++ api/src/main/java/com/example/todo/TodoApplication.java
@@ -3,11 +3,20 @@ package com.example.todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Todo application main class.
+ * Spring Boot application entry point.
+ */
 @SpringBootApplication
 public class TodoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoApplication.class, args);
-	}
+  /**
+   * Main method to start the Spring Boot application.
+   *
+   * @param args command line arguments
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(TodoApplication.class, args);
+  }
 
 }
